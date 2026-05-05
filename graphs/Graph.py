6e2efa70915cad9fdbb3595e7d8145726abe7193
Eren Akkoç <ern.akkc@gmail.com>
2026-05-06 01:55:44 +0300

    Add methods to manage graph edges in Graph class

--- graphs/Graph.py
+++ graphs/Graph.py
@@ -4,3 +4,15 @@ class Graph:
     def __init__(self):
         self.nodes = []
         self.edges = []
+
+    def add_edge(self, edge_from, edge_to, wheight:int = None):
+        self.nodes.append(edge_from, edge_to)
+        self.edges.append((edge_from, edge_to, wheight))
+
+    def add_edges(edges: list):
+        pass
+
+    
+G = Graph()
+
+G.add_edge("A", "B", 10)
